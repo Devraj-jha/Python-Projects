e748956d1703852378aaf0950377ef6701b7c836
Devraj-jha <devrajjha100806@gmail.com>
2025-11-01 11:34:29 +0530

    q1

---
+++ Python concepts/Core Python Foundations/qu1.py
@@ -0,0 +1,21 @@
+## Write a Python program that:
+
+# Takes two numbers from the user.
+
+# Prints the sum, difference, product, and quotient of the two numbers.
+
+# Checks if the first number is greater than the second and prints the result.
+
+# Checks if the first number is divisible by the second and prints True or False.
+
+
+a = float(input("Enter your 1st number : "))
+b = float(input("Enter your 2nd number : "))
+
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print( a > b )
+print( a % b == 0)
