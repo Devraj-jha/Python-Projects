66d6f6f6abf732a6620dc0b222a10bdbe41167dd
Devraj-jha <devrajjha100806@gmail.com>
2025-11-01 11:27:19 +0530

    python syntax done

---
+++ Python concepts/Core Python Foundations/pythonSyntax.py
@@ -0,0 +1,48 @@
+if True:
+    print("this works !! ")
+    #no curly bracket
+
+
+# variables are case sensetive
+Name = "alice "
+name = "dev"
+## different things
+
+#variables are names that store data
+#this data has some type which is called data type
+name = "string" # string
+age = 13 # int, 
+float_ = 3.24 # decimal values are called float 
+present = False ## true false I know that
+
+
+print(type(float_))
+
+# Variables
+a = 10
+b = 3
+
+# Arithmetic
+sum_ = a + b
+difference = a - b
+product = a * b
+quotient = a / b
+floor_div = a // b
+modulus = a % b
+power = a ** b
+
+print("Sum:", sum_)
+print("Difference:", difference)
+print("Product:", product)
+print("Quotient:", quotient)
+print("Floor Division:", floor_div)
+print("Modulus:", modulus)
+print("Power:", power)
+
+# Comparison
+print(a > b)  # True
+print(a == b) # False
+
+# Logical
+is_a_greater = a > b and b < 5
+print("Logical AND result:", is_a_greater)
