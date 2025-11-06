4e6b71c968d85030b69184ce1e38a9075e67373f
Devraj-jha <devrajjha100806@gmail.com>
2025-11-06 11:09:39 +0530

    what i remeber currently

---
+++ What_i_remeber.py
@@ -0,0 +1,51 @@
+print("hell owlrd")
+a = 23 # num 
+b = 'hi' # string 
+c = False #bool 
+d = None # no value
+
+if  a > 23 : 
+    print("greater than 23")
+else:
+    print("less than 23")
+
+for i in range(0,5):
+    print(i)
+n = 7
+while n > 5:
+    print(n)
+    n -= 1
+# I know conditional statements loops and conditional statements.
+# i know that i can loop over a list in python. 
+
+
+name = ' devraj jha '
+name = name.strip()
+print(name.capitalize())
+#I know there are various string methods... 
+
+list = [1,2,3,4,5]
+# i know data structure list in python. 
+for l in list: 
+    print(l)
+# i can loop over the loop.. 
+
+# i know f Strings 
+name1 = 'dev'
+age1 = 19
+
+print(f" {name1} is dj")
+
+#i know there are various modules exist in python that i can import and output. 
+
+#I KNOW HOW TO DECLARE a function in python. 
+def add(a,b):
+    return a + b
+def hello(): 
+
+    print("HLLO HOW ARE YOU!! ")
+
+
+print(add(3,4)) # i know how to call a fun. in python. 
+
+
