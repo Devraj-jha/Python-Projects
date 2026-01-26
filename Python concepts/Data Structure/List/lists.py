32229f9e31225294a4b9aa52020f1eb69addc811
Devraj-jha <devrajjha100806@gmail.com>
2026-01-26 09:42:01 +0530

    list

--- Python concepts/Data Structure/List/lists.py
+++ Python concepts/Data Structure/List/lists.py
@@ -1,34 +1,25 @@
-# LIst is a collections of items. ( element in a specific order)
+# list is ordered mutable collection of references
 
-# ordered 
-# mutable
-# heteroggeneous => can cotain different types of elements
+# ordered => has specific order
+# mutable => can change afterwards
+# can hold multiple items
+# gives refrecnces to objects
+nums = [1, 2, 3]
+names = ["Alice", "Bob"]
+mixed = [1, "hello", 3.5, True]
 
-numbers = [1, 2, 3, 4, 5]
-fruits = ["apple", "banana", "cherry"]
-mixed = [1, "apple", 3.14, True]
+#can have multiple data types together
 
-print(mixed[3])
-print(fruits[2].title())
-message = f"My favourite fruit is {fruits[0].title()}"
-print(message)
+## differnet method to use list
 
-#append => add element to the end of the list. 
+chars = list('ehllo') # h,e , l , l , 0
+nums = list(range(5))        # [0, 1, 2, 3, 4]
+nums = list(range(1, 10, 2)) # [1, 3, 5, 7, 9]
 
-motorcycles = ['honda', 'yamaha',' suzuki ']
+#indexing 
+nums = [10, 20, 30, 40]
 
-
-#insert  => INSERT element at the specific position.. 
-#name.insert(0, 'ducati')
-
-motorcycles.insert(2,'ducatii')
-
-print(motorcycles)
-
-# del = > delete particular item from the list. 
-del motorcycles[0]
-# pop()  => REMOVE LAST ELEMENT can be used to remvoe the element.
-
-
-
-print(len(motorcycles))
+nums[0]  # 10
+nums[1]  # 20
+nums[-1] # 40
+nums[-2] # 30
