27f288f5dd60c2ee9def33772bbf4be36fca3a49
Devraj-jha <devrajjha100806@gmail.com>
2026-01-26 08:54:34 +0530

    para_vs_argument

---
+++ Python concepts/Functions/para_vs_argu.py
@@ -0,0 +1,15 @@
+## parameters
+## => names used inside function definations
+
+def add (x,y):
+    return x + y
+
+# x, y here are function parameters
+
+y = add(10,20) ## 20, 30 are arguments
+
+
+# return does TWO things:
+# Sends a value back to the caller
+# Immediately ends the function
+
