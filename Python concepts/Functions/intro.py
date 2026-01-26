34b853020f84dc8cc4fffd50b98ca0466c62b4f4
Devraj-jha <devrajjha100806@gmail.com>
2026-01-26 08:50:44 +0530

    functions

---
+++ Python concepts/Functions/intro.py
@@ -0,0 +1,14 @@
+# A named block of logic that takes input, performs a transformation,
+# and optionally produces output.
+
+
+def hello():
+    print("Hello world") ## this is func. defined by def keywrod
+
+hello()
+# def function_name(parameters):
+#     # body
+#     return value
+
+#to cal a func. function_name();
+
