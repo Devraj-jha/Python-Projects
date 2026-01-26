e39d62231dc0ff20238700e3d6d67c4e321afaff
Devraj-jha <devrajjha100806@gmail.com>
2026-01-26 10:07:52 +0530

    tuple

--- Python concepts/Data Structure/Tuple/syntax_of_Tuple.py
+++ Python concepts/Data Structure/Tuple/syntax_of_Tuple.py
@@ -1,1 +1,25 @@
 #ordered collection of items.
+#An ordered, immutable collection of object references
+
+t = 1,2,3
+print(t)
+q1 = (1,2,3,4)
+print(q1)
+
+#empty
+t = ()
+point = (10, 20)
+x, y = point
+#left side destructres the tuple
+
+a = 10
+b = 20
+
+a,b = b,a
+
+a, *rest = (1, 2, 3, 4)
+# \Result:
+
+# a = 1
+
+# rest = [2, 3, 4]
---
+++ Python concepts/Data Structure/Tuple/tuples_in_func.py
@@ -0,0 +1,6 @@
+def divide(a,b):
+    return a//b, a% b
+
+q,r = divide(10,3)
+print(q)
+print(divide(10,3)) # returns a tuple
