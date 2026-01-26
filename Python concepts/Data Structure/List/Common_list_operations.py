b40cefea023416282dd633cfec1e7b3a10990046
Devraj-jha <devrajjha100806@gmail.com>
2026-01-26 09:48:30 +0530

    operations

---
+++ Python concepts/Data Structure/List/Common_list_operations.py
@@ -0,0 +1,18 @@
+nums = [1, 2]
+nums.append(3)
+# add a number in the end 
+
+nums.insert(1, 100)
+# adds at a particular index
+
+nums.remove(100)
+# remvoes first occurrence
+
+nums.pop()     # last element
+nums.pop(0)    # index 0
+#removes by index , if empty removes last element
+
+nums.clear() # I think clear the whole list
+
+len(nums) # lenght of the list
+
